a4f3ee019953179f13ca57d383087d7956146c71
leroy brown <leroybrown247@gmail.com>
2023-07-20 19:52:35 +1200

    Portfolio-Step-11 & 12: Project page built and functional

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
-let hamburger = document.querySelector('.hamburger-button')
-let times = document.querySelector('.times')
-let mobileNav = document.querySelector('.mobile-nav')
-let click = document.querySelector('.click')
-let click1 = document.querySelector('.click-1')
-let click2 = document.querySelector('.click-2')
-let click3 = document.querySelector('.click-3')
+let hamburger = document.querySelector('.hamburger-button');
+let times = document.querySelector('.times');
+let mobileNav = document.querySelector('.mobile-nav');
+let click = document.querySelector('.click');
+let click1 = document.querySelector('.click-1');
+let click2 = document.querySelector('.click-2');
+let click3 = document.querySelector('.click-3');
+let viewProjectBtn = document.getElementById('view-project-btn');
 
 hamburger.addEventListener('click', function() {
-    mobileNav.classList.add('open')
-})
+    mobileNav.classList.add('open');
+});
 
-times.addEventListener('click',function() {
-    mobileNav.classList.remove('open')
-})
+times.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+});
 
-click.addEventListener('click',function() {
-    mobileNav.classList.remove('open')
-})
+click.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+});
 
-click1.addEventListener('click',function() {
-    mobileNav.classList.remove('open')
-})
+click1.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+});
 
-click2.addEventListener('click',function() {
-    mobileNav.classList.remove('open')
-})
+click2.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+});
 
-click3.addEventListener('click',function() {
-    mobileNav.classList.remove('open')
-})
+click3.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+});
+
+viewProjectBtn.addEventListener('click', function() {
+    window.location.href = './projects/project1.html';
+});
